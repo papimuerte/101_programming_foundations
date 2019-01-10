3650d706aab2e4511067436c88c246e773120b82
papimuerte <nate@nateward.net>
2019-01-09 21:35:46 -0700

    lesson_3_easy_1

---
+++ lesson_3/easy_1/q_1.rb
@@ -0,0 +1,10 @@
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
+
+# As numbers.uniq doesn't mutate the array it would output:
+# 1
+# 2
+# 2
+# 3
---
+++ lesson_3/easy_1/q_4.rb
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5]
+
+numbers.delete_at(1)
+# this will delete the element at the specifed index
+numbers.delete(1)
+# this will delete all items from self that are equal to object
