9f6d0ba78198ff1e3a3332fea3a376386f186717
Nate <nate@Nates-MacBook-Pro.local>
2019-01-15 21:50:14 -0700

    Lesson 3

---
+++ lesson_3/hard_1/q_3.rb
@@ -0,0 +1,59 @@
+def mess_with_vars(one, two, three)
+  one = two
+  two = three
+  three = one
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+# => one is: one
+# => two is: two
+# => three is: three
+
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+# => one is: one
+# => two is: two
+# => three is: three
+
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+# => one is: two
+# => two is: three
+# => three is: one
